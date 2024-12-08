46d590aad237137fc75270ff8b40af27484884cb
mrEmman <ejbgatmaitan@gmail.com>
2024-12-08 21:27:53 +0800

    rename the tabs
    added article entry

---
+++ narrow-gate/narrow_gate/src/pages/Notes_from_the_wards.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Notes from the wards",
+    page_icon="👋",
+)
+st.markdown("# Notes from the wards")
+# st.sidebar.header("Notes from the Command-line")
+
+st.write("# Advent and Medical School - Second Sunday of Advent Dec 8, 2024")
+
+st.markdown(
+    """
+In the Philippines during this time, clerks often are asked to help with preparations for “Christmas parties” from blowing balloons to judging the talent show. The mood is rather festive; each duty seems lighter amidst the realities of sickness personal to each patient. 
+
+Paragraph about objective and subjective findings. 
+
+Generating a list of differential diagnoses and subsequently formulating a diagnostic plan. 
+
+What is often missed in medicine is the art of prognosis. Art and medicine being about practice. Prognosis being practiced varies from specialty to specialty. Certainly emergency medicine are prognostic experts on emergencies that can potentially kill the patient. Psychiatry less certain. (develop specialties that are more physical / physiological to more about the mind/spirit. 
+
+Prognosis is important yet underlooked. Spiritually with advent, prognosis of our souls requires joy because the prognosis is always good. As is often put as an epigraph in many a medical student presentations, “always begin with the end in mind”, a quote often attributed to Aristotle. Yet medical students are actually encouraged to put their biases about the cases aside and focus on the history taking and the objective facts that can come about. Since medical students are awaiting the beginning of their career, it suffices to say that the preparation involved in becoming a doctor involves a lot of sacrifice.
+
+Advent similarities as a period of waiting. Often old testament imagery points towards an uncertain future that doesn’t seem to make much sense if read alone. Rather, the old is revealed in the new and the new is hidden in the old (as Augustine says). Signs and prophecies seen in the old testament are fulfilled in the new. Likewise, the medical student’s esoteric studies in the cellular and molecular level of biology is revealed in the clinical cases that have genetic links. 
+
+
+Points/ discussion/ leading to conclusion: 
+Such a paradox actually fits the Church’s proclamation of the Gospel. In this period of Advent, the emphasis on hope yet is actually also a period of fasting and penance– a so called “penitential season.” What emerges from resolving this apparent dissonance is a deeper understanding of Joy. Much like the Church’s understanding of love, joy goes beyond emotion and 
+
+"""
+)
