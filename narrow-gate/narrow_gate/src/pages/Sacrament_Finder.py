e81cecbe965e73216949fb32ca809a52bbeafd4c
mrEmman <ejbgatmaitan@gmail.com>
2024-12-08 20:48:23 +0800

    Update Sacrament_Finder.py

--- narrow-gate/narrow_gate/src/pages/Sacrament_Finder.py
+++ narrow-gate/narrow_gate/src/pages/Sacrament_Finder.py
@@ -29,41 +29,48 @@ st.markdown("# Mass Finder Demo")
 # st.sidebar.header("Mass Finder Demo")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-churches = conn.read(
-    worksheet="churches",
-)
-masses = conn.read(
-    worksheet="masses",
-)
+df = conn.read()
 
-# sample_address = 'Skyway Twin Towers Condominium Capt. Javier st. Brgy Oranbo Pasig city'
-# search_range = 10
-# target_sched = '6:00 PM'
-current_datetime = datetime.datetime.now()
-sample_address = st.text_input("Your current location")
-search_range = st.number_input("Search radius(km)", value=5, step=1)
-target_sched = st.time_input("Target Schedule", step=1800).strftime('%I:%M %p')
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
 
-# st.table(churches.head(3))
-# st.table(masses.head(3))
-# st.markdown(target_sched)
 
-if st.button('Search'):
-    try:
-        church_results, mass_results = find_mass(churches, masses, sample_address, search_range, target_sched, current_datetime)
+# churches = conn.read(
+#     worksheet="churches",
+# )
+# masses = conn.read(
+#     worksheet="masses",
+# )
 
-        fig = px.scatter_map(church_results[church_results.church_name.isin(mass_results.church_name)],
-                            lat="lat",
-                            lon="long",
-                            hover_name="church_name", # display church name on hover
-                            text="church_name"
-                            )
-        sample_geocode = gmaps.geocode(sample_address)
-        fig.add_scattermap(lon=[sample_geocode[0]['geometry']['location']['lng']],
-                        lat=[sample_geocode[0]['geometry']['location']['lat']],
-                        name='my location'
-                        )
-        st.plotly_chart(fig)
-        st.table(mass_results[['schedule','church_name','church_address','Travel Time(Mins)','Arrival Time']])
-    except Exception as e:
-        st.markdown(e)
+# # sample_address = 'Skyway Twin Towers Condominium Capt. Javier st. Brgy Oranbo Pasig city'
+# # search_range = 10
+# # target_sched = '6:00 PM'
+# current_datetime = datetime.datetime.now()
+# sample_address = st.text_input("Your current location")
+# search_range = st.number_input("Search radius(km)", value=5, step=1)
+# target_sched = st.time_input("Target Schedule", step=1800).strftime('%I:%M %p')
+
+# # st.table(churches.head(3))
+# # st.table(masses.head(3))
+# # st.markdown(target_sched)
+
+# if st.button('Search'):
+#     try:
+#         church_results, mass_results = find_mass(churches, masses, sample_address, search_range, target_sched, current_datetime)
+
+#         fig = px.scatter_map(church_results[church_results.church_name.isin(mass_results.church_name)],
+#                             lat="lat",
+#                             lon="long",
+#                             hover_name="church_name", # display church name on hover
+#                             text="church_name"
+#                             )
+#         sample_geocode = gmaps.geocode(sample_address)
+#         fig.add_scattermap(lon=[sample_geocode[0]['geometry']['location']['lng']],
+#                         lat=[sample_geocode[0]['geometry']['location']['lat']],
+#                         name='my location'
+#                         )
+#         st.plotly_chart(fig)
+#         st.table(mass_results[['schedule','church_name','church_address','Travel Time(Mins)','Arrival Time']])
+#     except Exception as e:
+#         st.markdown(e)
